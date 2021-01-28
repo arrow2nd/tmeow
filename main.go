6803bb9bc777bca23d9c2770cb15859a57e18e67
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-01-28 23:22:23 +0900

    add: 設定ファイル周りの処理を追加

--- main.go
+++ main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
+	"github.com/arrow2nd/tmeow/api"
+	"github.com/arrow2nd/tmeow/config"
 	"github.com/arrow2nd/tmeow/view"
 	"github.com/rivo/tview"
 )
 
 func main() {
-	app := tview.NewApplication()
-	view.SharedConfig.App = app
+	// API・設定構造体作成
+	api := api.NewTwitterAPI()
+	cfg := config.NewConfig()
 
+	// 設定読込
+	if !cfg.Load() {
+		cfg.Cred.Token, cfg.Cred.Secret = api.Auth()
+		cfg.Save()
+	} else {
+		api.Init(cfg.Cred.Token, cfg.Cred.Secret)
+	}
+
+	// アプリケーション作成
+	app := tview.NewApplication()
+
+	// 画面作成
+	view.SharedConfig.App = app
+	view.SharedConfig.API = api.API
+	view.SharedConfig.Cfg = cfg
 	view := view.NewView()
 	view.Init()
 
+	// 実行
 	if err := app.Run(); err != nil {
 		panic(err)
 	}
--- view/config.go
+++ view/config.go
@@ -1,13 +1,21 @@
 package view
 
-import "github.com/rivo/tview"
+import (
+	"github.com/ChimeraCoder/anaconda"
+	"github.com/arrow2nd/tmeow/config"
+	"github.com/rivo/tview"
+)
 
 // Config 設定情報
 type Config struct {
 	App *tview.Application
+	API *anaconda.TwitterApi
+	Cfg *config.Config
 }
 
 // SharedConfig 共通設定
 var SharedConfig = Config{
 	App: nil,
+	API: nil,
+	Cfg: nil,
 }
